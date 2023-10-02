117faded0a7695fd73dea8cfa6b01b34e98d5c37
lucas costa <lnatanfrancacosta@gmail.com>
2023-10-02 18:46:31 -0300

    teste de integração

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,13 +1,35 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CatalogServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void whenPostBooksCreateNewBook() {
+        var expectedBook = new Book("1234567890", "title of the book", "test author", 9.90);
+
+        webTestClient
+                .post()
+                .uri("/books")
+                .bodyValue(expectedBook)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Book.class).value(actualBook -> {
+                    assertThat(actualBook).isNotNull();
+                    assertThat(actualBook.isbn())
+                            .isEqualTo(expectedBook.isbn());
+                });
+    }
+
 
 }
